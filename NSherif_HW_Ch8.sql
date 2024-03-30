bd48b74e05ea62704170bda0bb7a68102e4a0318
nsherif2 <62778803+nsherif2@users.noreply.github.com>
2024-03-30 13:35:16 -0400

    NSherif_HW_Ch8.sql

--- NSherif_HW_Ch8.sql
+++ NSherif_HW_Ch8.sql
@@ -1,10 +1,7 @@
 --Assignment: Homework Chapter 8
 --Short Description: This SQL homework involves six exercises focusing on SQL queries to manipulate and format data from "Products" and "Orders" tables. Tasks include formatting prices, converting date types, calculating discounts, and masking credit card numbers. Each exercise is designed to practice specific SQL functions like TO_CHAR, CAST, ROUND, and SUBSTR for data presentation and manipulation.
 --File: ExampleCh8.sql
---Course: CMSC246
---Student: Nebil Sherif
---Professor G Grinberg
---Due Date: 10/11/21
+
 
 
 /*
@@ -17,18 +14,7 @@ SELECT
   TO_CHAR(list_price, 'L999G999D99') AS formatted_price
 FROM
   Products;
-  /*
-699	              $699.00
-1199	            $1,199.00
-2517	            $2,517.00
-489.99	              $489.99
-299	              $299.00
-415	              $415.00
-799.99	              $799.99
-499.99	              $499.99
-699.99	              $699.99
-799.99	              $799.99
-  */
+
 
 /*2.Write a SELECT statement that returns these columns from the Products table:
 The date_added column
@@ -39,19 +25,7 @@ SELECT
   CAST(date_added AS VARCHAR2(9)) AS date_added_varchar
 FROM
   Products;
-  /*
-30-OCT-11	30-OCT-11
-05-DEC-11	05-DEC-11
-04-FEB-12	04-FEB-12
-01-JUN-12	01-JUN-12
-30-JUL-12	30-JUL-12
-30-JUL-12	30-JUL-12
-01-JUN-12	01-JUN-12
-30-JUL-12	30-JUL-12
-30-JUL-12	30-JUL-12
-30-JUL-12	30-JUL-12
-  */
-
+ 
 
 /*
 3.Write a SELECT statement that returns these columns from the Products table:
@@ -66,18 +40,7 @@ SELECT
   ROUND((list_price * (discount_percent / 100)), 2) AS discount_amount
 FROM
   Products;
-  /*
-699	30	209.7
-1199	30	359.7
-2517	52	1308.84
-489.99	38	186.2
-299	0	0
-415	39	161.85
-799.99	30	240
-499.99	25	125
-699.99	30	210
-799.99	15	120
-  */
+ 
 
 
 /*4.	Write a SELECT statement that returns these columns from the Orders table:
@@ -93,17 +56,7 @@ SELECT
   TO_CHAR(order_date, 'MM/DD/YY HH24:MI') AS formatted_date_time
 FROM
   Orders;
-  /*
-28-MAR-12	2012	MAR-28-2012	09:40 AM	03/28/12 09:40
-28-MAR-12	2012	MAR-28-2012	11:23 AM	03/28/12 11:23
-29-MAR-12	2012	MAR-29-2012	09:44 AM	03/29/12 09:44
-30-MAR-12	2012	MAR-30-2012	03:22 PM	03/30/12 15:22
-31-MAR-12	2012	MAR-31-2012	05:43 AM	03/31/12 05:43
-31-MAR-12	2012	MAR-31-2012	06:37 PM	03/31/12 18:37
-01-APR-12	2012	APR-01-2012	11:11 PM	04/01/12 23:11
-02-APR-12	2012	APR-02-2012	11:26 AM	04/02/12 11:26
-03-APR-12	2012	APR-03-2012	12:22 PM	04/03/12 12:22
-  */
+ 
 
 /*
 5.	Write a SELECT statement that returns these columns from the Orders table:
@@ -117,17 +70,7 @@ SELECT
   'XXXX-XXXX-XXXX-' || SUBSTR(card_number, -4) AS masked_card_number
 FROM
   Orders;
-  /*
-4111111111111111	16	1111	XXXX-XXXX-XXXX-1111
-4012888888881881	16	1881	XXXX-XXXX-XXXX-1881
-4111111111111111	16	1111	XXXX-XXXX-XXXX-1111
-378282246310005 	16	005 	XXXX-XXXX-XXXX-005 
-4111111111111111	16	1111	XXXX-XXXX-XXXX-1111
-6011111111111117	16	1117	XXXX-XXXX-XXXX-1117
-5555555555554444	16	4444	XXXX-XXXX-XXXX-4444
-4012888888881881	16	1881	XXXX-XXXX-XXXX-1881
-4111111111111111	16	1111	XXXX-XXXX-XXXX-1111
-  */
+ 
 
 /*
 6.Write a SELECT statement that returns these columns from the Orders table:
@@ -143,11 +86,4 @@ FROM
   Orders
 WHERE
   EXTRACT(YEAR FROM order_date) = 2012 AND EXTRACT(MONTH FROM order_date) = 3;
- /*
-1	28-MAR-12	30-MAR-12	30-MAR-12	2.24471064814814814814814814814814814815
-2	28-MAR-12	30-MAR-12	29-MAR-12	1.06173611111111111111111111111111111111
-3	29-MAR-12	31-MAR-12	31-MAR-12	1.97688657407407407407407407407407407407
-4	30-MAR-12	01-APR-12	03-APR-12	4.04849537037037037037037037037037037037
-5	31-MAR-12	02-APR-12	02-APR-12	2.3597337962962962962962962962962962963
-6	31-MAR-12	02-APR-12		
-*/
+
